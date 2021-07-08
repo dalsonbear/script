6e54957d400d7556b0fa030089b8671adf7b300a
dalsonbear <62308484+dalsonbear@users.noreply.github.com>
2021-07-08 08:31:36 +0800

    Update jdJxncTokens.js

--- jdJxncTokens.js
+++ jdJxncTokens.js
@@ -6,8 +6,8 @@
 // 每个账号 token 是一个 json，示例如下
 // {"farm_jstoken":"749a90f871adsfads8ffda7bf3b1576760","timestamp":"1610165423873","phoneid":"42c7e3dadfadsfdsaac-18f0e4f4a0cf"}
 let JxncTokens = [
-  '{"smp":"5377c91ad125b2b8689602957487fb7d","active":"jdnc_1_3yuanhongshu210422_2","joinnum":1}',//账号一的京喜农场token
-  '{"smp":"cd5de16b0239aa796ca948e4b05a59e2","active":"jdnc_1_1yuantiangua210616_2","joinnum":1}',//账号二的京喜农场token
+  '{\"smp\":\"5377c91ad125b2b8689602957487fb7d\",\"active\":\"jdnc_1_1yuanqicheng210616_2\",\"joinnum\":1}',//账号一的京喜农场token
+  '',//账号二的京喜农场token
 ]
 // 判断github action里面是否有京喜农场 token 
 if (process.env.JXNCTOKENS) {
